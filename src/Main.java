f4edf2234f70923bbf4321e8d7d14c441e1a70df
piioni <rangelc.juane@gmail.com>
2025-01-02 20:10:19 +0100

    Game functional, asking if player would like to play another round and change the difficulty

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,38 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner sc_main = new Scanner(System.in);
 
         System.out.println("Welcome to the Number Guessing Game!");
         System.out.println("I'm thinking of a number between 1 and 100.");
 
+        int difficulty = getDifficulty();
+        playGame(difficulty);
+
+        boolean playAgain = true;
+        while (playAgain) {
+            System.out.println("Do you want to play again? (yes/no)");
+            String play = sc_main.next();
+            if (play.equalsIgnoreCase("no")) {
+                playAgain = false;
+            } else {
+                System.out.println("Do you want to change the difficulty? (yes/no)");
+                String change = sc_main.next();
+                if (change.equalsIgnoreCase("yes")) {
+                    difficulty = getDifficulty();
+                    playGame(difficulty);
+                } else {
+                    playGame(difficulty);
+                }
+            }
+        }
+        sc_main.close();
+    }
+
+
+    public static int getDifficulty() {
         int difficulty = 0;
+        Scanner sc_dificultad = new Scanner(System.in);
         do {
             try {
                 System.out.println("Please enter the difficulty level");
@@ -15,46 +41,45 @@ public class Main {
                 System.out.println("2. Medium (5 guesses)");
                 System.out.println("3. Hard (3 guesses)");
                 System.out.print("Enter your choice: ");
-                difficulty = sc.nextInt();
+                difficulty = sc_dificultad.nextInt();
             } catch (Exception e) {
                 System.out.println("Invalid input. Please enter a number between 1 and 3.");
-                sc.nextLine();
+                sc_dificultad.nextLine();
             }
         } while (difficulty < 1 || difficulty > 3);
+        return difficulty;
+    }
 
+    public static void playGame(int difficulty) {
+        int guesses = 0;
         switch (difficulty) {
             case 1:
                 System.out.println("You've chosen Easy. You have 10 guesses!");
                 System.out.println("Let's start the game!, Good Luck!");
-                playGame(10);
+                guesses = 10;
                 break;
             case 2:
                 System.out.println("Great you've chosen Medium. You have 5 guesses!");
                 System.out.println("Let's start the game!, Good Luck!");
-                playGame(5);
+                guesses = 5;
                 break;
             case 3:
                 System.out.println("You're brave! You've chosen Hard. You have 3 guesses!");
                 System.out.println("Let's start the game!, Good Luck!");
-                playGame(3);
+                guesses = 3;
                 break;
             default:
                 System.out.println("Invalid input. Please enter a number between 1 and 3.");
         }
 
-
-
-    }
-
-    public static void playGame(int i) {
         int tries = 0;
         int number = (int) (Math.random() * 100) + 1;
-        Scanner sc = new Scanner(System.in);
+        Scanner sc_game = new Scanner(System.in);
         boolean hasWon = false;
-        while (tries < i) {
+        while (tries < guesses) {
             System.out.println("Please enter your guess: ");
             try {
-                int guess = sc.nextInt();
+                int guess = sc_game.nextInt();
                 tries++;
                 if (guess < number) {
                     System.out.println("Your guess is too low.");
@@ -66,15 +91,14 @@ public class Main {
                 }
             } catch (Exception e) {
                 System.out.println("Invalid input. Please enter a number between 1 and 100.");
-                sc.nextLine();
+                sc_game.nextLine();
             }
-
-
         }
         if (hasWon) {
             System.out.println("Congratulations! You've guessed the correct number in " + tries + " tries.");
         } else {
             System.out.println("Game Over! The correct number was " + number);
         }
+
     }
 }
